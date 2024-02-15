5307644303a538aa9b3d4d5571cf0c261f0ec57a
Shubham Chauhan <shubhamsc9504@gmail.com>
2024-02-16 00:28:17 +0530

    Ctreated chess dir

---
+++ Chess.java
@@ -0,0 +1,7 @@
+public class Chess{
+    public static void main(String[] args) {
+        System.out.println("Lets strt the game");
+
+        newGame game=new newGame();
+    }
+}
---
+++ newGame.java
@@ -0,0 +1,13 @@
+public class newGame {
+
+    newGame(){
+        System.out.println("Game Created");
+        System.out.println("start game");
+        startGame();
+    }
+
+    void startGame(){
+        System.out.println("Game started");
+    }
+
+}
